ba81937671979fdfebc67082887eac9f4d3d32fc
kimjunha1231 <tto9011@naver.com>
2026-05-26 14:35:02 +0900

    feat : vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <HelmetProvider>
         <App />
+        <Analytics />
       </HelmetProvider>
     </QueryClientProvider>
   </StrictMode>,
